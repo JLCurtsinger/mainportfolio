ca7f136d96784a53fa9930e1dc050af43601e27b
JLCurtsinger <98992595+JLCurtsinger@users.noreply.github.com>
2025-07-29 10:09:51 -0700

    added a linked to my linkedin profile at the bottom of the site

--- src/components/contact/SocialLinks.tsx
+++ src/components/contact/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import { Github, Mail } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 
 export const SocialLinks = () => {
   return (
@@ -10,6 +10,15 @@ export const SocialLinks = () => {
         className="p-2 rounded-full bg-accent/5 hover:bg-accent/10 transition-colors"
       >
         <Github className="w-6 h-6" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/justin-curtsinger-5a06b9345/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="LinkedIn"
+        className="p-2 rounded-full bg-accent/5 hover:bg-accent/10 transition-colors"
+      >
+        <Linkedin className="w-6 h-6" />
       </a>
       <a
         href="mailto:cjcurtsinger502@aol.com"
